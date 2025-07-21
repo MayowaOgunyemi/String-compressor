14a002f0fa11a9230daf54de6785776c99a831e6
engrmayoronline <engrmayoronline@gmail.com>
2025-07-21 13:36:13 +0100

    Updated index file

--- index.php
+++ index.php
@@ -4,11 +4,6 @@ require 'vendor/autoload.php';
 
 use Vendor\Seven20StringCompressor\StringCompressor;
 
-// $compressor = new StringCompressor();
-
-// $input = "aabbccaa";
-// echo "Original: $input\n";
-// echo "Compressed: " . $compressor->compress($input) . "\n";
 try {
     $compressor = new StringCompressor();
     $input = "aaabbccddddddefghhhiiiiihhhxxxxaaaaffffjjjjeeeeeeeeeeeeeeeeePPPPPPaaaaAAAA";
